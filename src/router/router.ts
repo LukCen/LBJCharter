80f09f37ad39dda32f8aaeaed6e6747e8025c0a2
lbjcena <lbjcena@gmail.com>
2026-01-17 23:25:25 +0100

    router removed - not in use

--- src/router/router.ts
+++
@@ -1,7 +0,0 @@
-import ChartDataForm from "@/components/ChartDataForm.vue";
-import { createMemoryHistory, createRouter } from "vue-router";
-
-const routes = [
-  { path: '/', component: ChartDataForm },
-  { path: '/upload', component: '' }
-]
