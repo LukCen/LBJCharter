fcefa9ee1118aef20ea079a530d8ef203f54fe0b
lbjcena <lbjcena@gmail.com>
2026-01-27 23:39:35 +0100

    CommandItem component - styling improvements to highlighted and highlighted:hover state

--- src/components/ui/command/CommandItem.vue
+++ src/components/ui/command/CommandItem.vue
@@ -60,7 +60,7 @@ onUnmounted(() => {
 
 <template>
   <ListboxItem v-if="isRender" v-bind="forwarded" :id="id" ref="itemRef" data-slot="command-item"
-    :class="cn('data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*=\'size-\'])]:size-4', props.class)"
+    :class="cn('data-[highlighted]:border-primary data-[highlighted]:border-2  data-[highlighted]:text-accent-foreground data-[highlighted]:hover:text-secondary [&_svg:not([class*=\'text-\'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*=\'size-\'])]:size-4', props.class)"
     @select="() => {
       filterState.search = ''
     }">
